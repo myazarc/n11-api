48f955e744747aebb8128dead9683b7cffbc95d4
myazarc <myazarc@yandex.com>
2022-03-07 20:01:17 +0300

    version updated

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import CategoryService from "./n11/Services/CategoryService";
 import ProductService from "./n11/Services/ProductService";
+import ShipmentService from "./n11/Services/ShipmentService";
 
-const n11 = { CategoryService, ProductService };
+const n11 = { CategoryService, ProductService, ShipmentService };
 
 export default n11;
-export { CategoryService, ProductService };
+export { CategoryService, ProductService, ShipmentService };
 module.exports = n11;
