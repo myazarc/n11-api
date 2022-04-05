2eb43cfb91c8b41517200977508b3ae308cb9204
myazarc <myazarc@yandex.com>
2022-04-05 21:13:07 +0300

    edited orderService

--- src/n11/Services/OrderService.ts
+++ src/n11/Services/OrderService.ts
@@ -1,34 +1,17 @@
-import {
-  IStartSellingProductBySellerCodeRequest,
-  IStartSellingProductBySellerCodeResponse,
-  IStopSellingProductBySellerCodeRequest,
-  IStopSellingProductBySellerCodeResponse,
-} from "../../Interfaces/IProductSellingService";
 import SoapBase from "../Base";
+import { IOrderListRequest, IOrderListResponse } from "../../Interfaces/IOrderService";
 
-class ProductSellingService extends SoapBase {
+class OrderService extends SoapBase {
   constructor(appKey: string, appSecret: string) {
     super(appKey, appSecret);
     this.SERVICE_URL = "https://api.n11.com/ws/OrderService.wsdl";
   }
 
-  public stopSellingProductBySellerCode(productSellerCode: string): Promise<[IStopSellingProductBySellerCodeResponse, string, any, string]> {
-    const request: IStopSellingProductBySellerCodeRequest = {
-      auth: this.getAuth(),
-      productSellerCode,
-    };
-
-    return this._.StopSellingProductBySellerCodeAsync(request);
-  }
-
-  public startSellingProductBySellerCode(productSellerCode: string): Promise<[IStartSellingProductBySellerCodeResponse, string, any, string]> {
-    const request: IStartSellingProductBySellerCodeRequest = {
-      auth: this.getAuth(),
-      productSellerCode,
-    };
+  public orderList(request: IOrderListRequest): Promise<[IOrderListResponse, string, any, string]> {
+    request.auth = this.getAuth();
 
     return this._.StopSellingProductBySellerCodeAsync(request);
   }
 }
 
-export default ProductSellingService;
+export default OrderService;
