2c243ba61237cb4d468041f550ab340c016d8029
myazarc <myazarc@yandex.com>
2022-04-05 21:37:01 +0300

    added orderService in package

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,10 @@ import ProductService from "./n11/Services/ProductService";
 import ProductStockService from "./n11/Services/ProductStockService";
 import ProductSellingService from "./n11/Services/ProductSellingService";
 import ShipmentService from "./n11/Services/ShipmentService";
+import OrderService from "./n11/Services/OrderService";
 
-const n11 = { CategoryService, ProductService, ShipmentService, ProductStockService, ProductSellingService };
+const n11 = { CategoryService, ProductService, ShipmentService, ProductStockService, ProductSellingService, OrderService };
 
 export default n11;
-export { CategoryService, ProductService, ShipmentService, ProductStockService, ProductSellingService };
+export { CategoryService, ProductService, ShipmentService, ProductStockService, ProductSellingService, OrderService };
 module.exports = n11;
